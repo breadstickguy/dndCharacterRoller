3807a24aef1f6eded39a6a781f2cd0ef869c1732
JJ Brenner <brenner.jj@gmail.com>
2020-03-07 14:27:10 -0700

    roll/rollMulti tests and code

--- test/util/dice.test.js
+++ test/util/dice.test.js
@@ -17,4 +17,30 @@ describe('Dice module', () => {
 			expect(rollFour).toBeLessThanOrEqual(4);
 		});
 	});
+	describe('multiRoll', () => {
+		it('should return an array', () => {
+			const rollMulti = dice.rollMulti();
+			expect(Array.isArray(rollMulti)).toBeTruthy();
+		});
+		it('should only run once if no params', () => {
+			const rollMulti = dice.rollMulti();
+			expect(rollMulti.length).toEqual(1);
+		});
+		it('should roll the proper type of die', () => {
+			const rollMulti = dice.rollMulti(2, 12);
+			expect(rollMulti.length).toEqual(2);
+			expect(rollMulti[0]).toBeGreaterThanOrEqual(1);
+			expect(rollMulti[0]).toBeLessThanOrEqual(12);
+			expect(rollMulti[1]).toBeGreaterThanOrEqual(1);
+			expect(rollMulti[1]).toBeLessThanOrEqual(12);
+		});
+		it('should roll the proper number of dice', () => {
+			const rollMulti = dice.rollMulti(4);
+			const rollMultiSix = dice.rollMulti(6);
+			const rollMultiTen = dice.rollMulti(10);
+			expect(rollMulti.length).toEqual(4);
+			expect(rollMultiSix.length).toEqual(6);
+			expect(rollMultiTen.length).toEqual(10);
+		});
+	});
 });
--- util/dice.js
+++ util/dice.js
@@ -1,6 +1,14 @@
 
 const dice = {
-    roll: (sides = 20) => Math.floor(Math.random() * sides + 1)
+    roll: (sides = 20) => Math.floor(Math.random() * sides + 1),
+    rollMulti: (numDice = 1, sides) => {
+        const rolls = [];
+        for(let i = 0; i < numDice; i++) {
+            rolls.push(dice.roll(sides));
+        }
+        return rolls;
+    }
+
 }
 
 module.exports = dice;
